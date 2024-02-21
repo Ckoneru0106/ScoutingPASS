2bbed73a6cf8a3b601cd3c714fe001ad3e3cc4d2
klhansen4 <46847874+klhansen4@users.noreply.github.com>
2024-02-20 21:06:06 -0500

    Update crescendo_config.js
    changed default match from 2023tnkn to 2023milan for testing purposes

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023tnkn",
+      "defaultValue": "2023milan",
       "required": "true"
     },
     { "name": "Match Level",
